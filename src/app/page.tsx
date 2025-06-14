41cc1d6cfa5b29d4030bd0183744fac294687f89
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-06-14 14:35:17 -0300

    create PostsList componet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import { Header } from "@/components/Header";
+import { PostsList } from '@/components/PostsList';
+import { SpinLoader } from '@/components/SpinLoader';
+import { Suspense } from 'react';
 
 export default async function HomePage() {
-  console.log('HomePage');
 
   return (
     <div>
-      <Header />
+      <header className='text-6xl font-bold text-center py-8'>
+        <h1>Header</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+      <PostsList></PostsList>
+      </Suspense>
+
+      <footer className='text-6xl font-bold text-center py-8'>
+        <p>Footer</p>
+      </footer>
     </div>
   );
 }
