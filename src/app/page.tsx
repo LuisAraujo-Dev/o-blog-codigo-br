7bd70af47abb4d5015203847609e533dfe1a9fab
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-06-15 18:08:35 -0300

    create Container Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
+import { Container } from '@/components/Container';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
-
   return (
-    <div>
-      <header className='text-6xl font-bold text-center py-8'>
-        <h1>Header</h1>
+    <Container>
+      <header>
+        <h1 className='text-6xl font-bold text-center py-8'>Header</h1>
       </header>
 
       <Suspense fallback={<SpinLoader />}>
-      <PostsList></PostsList>
+        <PostsList></PostsList>
       </Suspense>
 
-      <footer className='text-6xl font-bold text-center py-8'>
-        <p>Footer</p>
+      <footer>
+        <p className='text-6xl font-bold text-center py-8'>Footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
