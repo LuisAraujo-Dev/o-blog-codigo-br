8974d2282eda26481150ff6637eb34f70dd35976
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-06-24 11:26:03 -0300

    create findAllPublicPosts and used cache

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,11 +1,12 @@
 import { PostCoverImage } from '../PostCoverImage';
 import { PostSummary } from '../PostSummary';
+import { findAllPublicPosts } from '@/lib/post/queries';
 
-export function PostFeatured() {
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts()
+  const post = posts[0]
 
-
-  const slug = 'qualquer';
-  const postLink = `/post/${slug}`;
+  const postLink = `/post/${post.slug}`;
 
   return (
     <section className={'grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'}>
@@ -16,13 +17,13 @@ export function PostFeatured() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: '/images/bryen_0.png',
-          alt: 'Alt da imagem',
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
 
-        <PostSummary postHeading='h1' postLink={postLink} createdAt={'2023-06-23T15:30:00Z'} title={'title post'} excerpt={'sdaad asdasdsa sdadads asdadsa'} />
+        <PostSummary postHeading='h1' postLink={postLink} createdAt={post.createdAt} title={post.title} excerpt={post.excerpt} />
 
     </section>
   );
