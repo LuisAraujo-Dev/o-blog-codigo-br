e246d8aa2b5efc887c13419fb7bf15a59647aa91
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-06-28 13:05:19 -0300

    styling footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,12 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export function Footer() {
   return (
     <footer className='pb-16 text-center'>
-      <span><p>Copyright &copy; {new Date().getFullYear()} - <Link href={'/'}> The Blog</Link></p></span>
+      <p>
+        <span>Copyright &copy; {new Date().getFullYear()} - </span>
+        <Link href='/'>Luís Araújo</Link>
+      </p>
     </footer>
   );
 }
