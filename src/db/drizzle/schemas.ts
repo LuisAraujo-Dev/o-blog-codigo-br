672cb544d956ebba17b3580fd752368ea23bc964
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-06-30 15:05:36 -0300

    create first schema and type

--- src/db/drizzle/schemas.ts
+++ src/db/drizzle/schemas.ts
@@ -1,5 +1,18 @@
-import { sqliteTable } from "drizzle-orm/sqlite-core";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const postTable = sqliteTable('posts', {
-  id: ,
+  id: text('id').primaryKey(),
+  slug: text('slug').notNull().unique(),
+  title: text('title').notNull(),
+  author: text('author').notNull(),
+  excerpt: text('excerpt').notNull(),
+  content: text('content').notNull(),
+  coverImageUrl: text('cover-image-url').notNull(),
+  published: integer('published', {mode: 'boolean'}).notNull(),
+  createAt: text('createAt').notNull(),
+  updateAt: text('updateAt').notNull(),
 })
+
+export type postTableSelectMode = InferSelectModel<typeof postTable>
+export type postTableInsertMode = InferInsertModel<typeof postTable>
