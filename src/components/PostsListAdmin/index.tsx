44409b20c46274c62a8f9651cd76b5acb7b66ef4
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-18 16:08:42 -0300

    dialog box layout

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -26,10 +26,51 @@ export default async function PostsListAdmin() {
               </span>
             )}
 
-            <DeletePostButton id={post.id} title={post.title}/>
+            <DeletePostButton id={post.id} title={post.title} />
           </div>
         );
       })}
+      <div
+        className={clsx(
+          'fixed z-50 inset-0 bg-black/50 backdrop-blur-sm',
+          'flex justify-center items-center',
+        )}
+      >
+        <div
+          className={clsx(
+            'bg-slate-100 p-6 rounded-lg max-w-2xl mx-6',
+            'flex flex-col gap-6',
+            'shadow-lg shadow-black/30 text-center'
+          )}
+        >
+          <h3 className='text-xl font-extrabold'>Title</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, obcaecati.
+          </p>
+          <div className='flex items-center justify-around'>
+            <button
+              className={clsx(
+                'bg-slate-200 hover:bg-slate-300 transition text-slate-950',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+              autoFocus
+            >
+              Cancelar
+            </button>
+            <button
+              className={clsx(
+                'bg-blue-500 hover:bg-blue-600 transition text-blue-50',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
