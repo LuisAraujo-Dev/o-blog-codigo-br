87b76dbdbe28f311f1d1f3dd4426024a61712e51
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-28 18:42:37 -0300

    configuring and applying toastify on delete post

--- src/components/ToastifyContainer/index.tsx
+++ src/components/ToastifyContainer/index.tsx
@@ -1,19 +1,20 @@
-'use client'
+'use client';
 
-import { Bounce, ToastContainer } from "react-toastify"
+import { Bounce, ToastContainer } from 'react-toastify';
 
 export function ToastifyContainer() {
-  return <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick={false}
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
-transition={Bounce}
-/>
+  return (
+    <ToastContainer
+      position='top-center'
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={true}
+      rtl={false}
+      pauseOnFocusLoss
+      pauseOnHover
+      theme='light'
+      transition={Bounce}
+    />
+  );
 }
