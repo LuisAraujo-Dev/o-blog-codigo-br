fef4d397b1b5b04777e72fe5a7f69f2a6b252d39
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-28 20:24:05 -0300

    styling MenuAdmin

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,11 +1,34 @@
-import Link from "next/link";
+import clsx from 'clsx';
+import { FileTextIcon, HouseIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export function MenuAdmin() {
-  return <nav>
-    <a href="/" target="_blank">
-      Home
-    </a>
+  const navClass = clsx(
+    `bg-slate-900 text-slate-100 rounded-lg`,
+    'flex flex-col mb-8',
+    'sm:flex-row sm:flex-wrap',
+    'overflow-hiden',
+    'h-10',
+  );
+  const linkClass = clsx(
+    '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
+    'flex items-center justify-start gap-2',
+    'trasition hover:bg-slate-800 hover:rounded-lg',
+    'h-10',
+    'shrink-0',
+  );
 
-    <Link href={'/admin/post'} >Posts</Link>
-  </nav>
+  return (
+    <nav className={navClass}>
+      <a className={linkClass} href='/' target='_blank'>
+        <HouseIcon />
+        Home
+      </a>
+
+      <Link className={linkClass} href={'/admin/post'}>
+        <FileTextIcon />
+        Posts
+      </Link>
+    </nav>
+  );
 }
