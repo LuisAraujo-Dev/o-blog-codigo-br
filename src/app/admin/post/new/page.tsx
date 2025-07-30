7c1b45f73b3e3314bad3f59a98cfb55f18558625
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-30 16:57:05 -0300

    aplicated Button components in the new post

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,11 +1,19 @@
-import { Button } from "@/components/Button";
+import { Button } from '@/components/Button';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
-  return <div className='py-16'>
-    <Button>Confirma</Button>
-    <Button>Confirma</Button>
-    <Button>Confirma</Button>
-  </div>;
+  return (
+    <div className='py-16 flex gap-4 flex-wrap'>
+      <Button variant='default' size='sm'>
+        Confirma
+      </Button>
+      <Button variant='ghost' size='md'>
+        Confirma
+      </Button>
+      <Button variant='danger' size='lg'>
+        Confirma
+      </Button>
+    </div>
+  );
 }
