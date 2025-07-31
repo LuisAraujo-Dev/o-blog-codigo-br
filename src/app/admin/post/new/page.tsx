c8472569458310b825d597f1eafdfa700514f7c0
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-30 21:34:38 -0300

    styling and using Button components in Dialog

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -4,16 +4,29 @@ export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16 flex gap-4 flex-wrap'>
-      <Button variant='default' size='sm'>
-        Confirma
-      </Button>
-      <Button variant='ghost' size='md'>
-        Confirma
-      </Button>
-      <Button variant='danger' size='lg'>
-        Confirma
-      </Button>
-    </div>
+    <>
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm'>
+          Confirma
+        </Button>
+        <Button variant='ghost' size='md'>
+          Confirma
+        </Button>
+        <Button variant='danger' size='lg'>
+          Confirma
+        </Button>
+      </div>
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm' disabled>
+          Confirma
+        </Button>
+        <Button variant='ghost' size='md' disabled>
+          Confirma
+        </Button>
+        <Button variant='danger' size='lg' disabled>
+          Confirma
+        </Button>
+      </div>{' '}
+    </>
   );
 }
