37c4838b47616a54851b7235e233f186a4e82867
Luis <luisaraujo.dev@gmail.com>
2025-08-05 18:53:41 -0300

    create InputCheckbox component

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/Button';
+import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
 
 export const dynamic = 'force-dynamic';
@@ -6,6 +6,8 @@ export const dynamic = 'force-dynamic';
 export default async function AdminPostNewPage() {
   return (
     <div className='flex flex-col gap-6'>
+      <InputCheckbox labelText='Checkbox'/>
+
       <InputText placeholder='Digite seu nome' id={''} labelText={'Label'} />
       <InputText placeholder='Digite seu nome' id={''} labelText={''} />
     </div>
