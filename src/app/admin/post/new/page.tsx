a68739728710c114ef2910a39e15f522274dfc5b
Luis <luisaraujo.dev@gmail.com>
2025-08-05 19:01:05 -0300

    add form and Button on the new post page

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/Button';
 import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
 
@@ -5,11 +6,17 @@ export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='flex flex-col gap-6'>
-      <InputCheckbox labelText='Checkbox'/>
+    <form action="" className='mb-16'>
+      <div className='flex flex-col gap-6'>
+        <InputCheckbox labelText='Checkbox' />
 
-      <InputText placeholder='Digite seu nome' id={''} labelText={'Label'} />
-      <InputText placeholder='Digite seu nome' id={''} labelText={''} />
-    </div>
+        <InputText placeholder='Digite seu nome' id={''} labelText={'Label'} />
+        <InputText placeholder='Digite seu nome' id={''} labelText={''} />
+
+        <div className='mt-4'>
+          <Button type='submit' size='md'>Enviar</Button>
+        </div>
+      </div>
+    </form>
   );
 }
