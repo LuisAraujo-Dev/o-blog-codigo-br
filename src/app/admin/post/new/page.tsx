642cce9ca0ccbf8a1f8de5d1f5bd0def0ed22da7
Luis <luisaraujo.dev@gmail.com>
2025-08-05 19:16:03 -0300

    create ManegePostForm component and moved the form into it

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,22 +1,9 @@
-import { Button } from '@/components/Button';
-import { InputCheckbox } from '@/components/InputCheckbox';
-import { InputText } from '@/components/InputText';
+import { ManegePostForm } from '@/components/Admin/ManegePostForm';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <form action="" className='mb-16'>
-      <div className='flex flex-col gap-6'>
-        <InputCheckbox labelText='Checkbox' />
-
-        <InputText placeholder='Digite seu nome' id={''} labelText={'Label'} />
-        <InputText placeholder='Digite seu nome' id={''} labelText={''} />
-
-        <div className='mt-4'>
-          <Button type='submit' size='md'>Enviar</Button>
-        </div>
-      </div>
-    </form>
+    <ManegePostForm/>
   );
 }
---
+++ src/components/Admin/ManegePostForm/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { Button } from "@/components/Button";
+import { InputCheckbox } from "@/components/InputCheckbox";
+import { InputText } from "@/components/InputText";
+
+
+export function ManegePostForm() {
+  return (
+    <form action="" className='mb-16'>
+          <div className='flex flex-col gap-6'>
+            <InputCheckbox labelText='Checkbox' />
+    
+            <InputText placeholder='Digite seu nome' id={''} labelText={'Label'} />
+            <InputText placeholder='Digite seu nome' id={''} labelText={''} />
+    
+            <div className='mt-4'>
+              <Button type='submit' size='md'>Enviar</Button>
+            </div>
+          </div>
+        </form>
+  )
+}
