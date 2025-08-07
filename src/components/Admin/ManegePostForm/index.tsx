f1460fb34b0a6b8a3379fa23507d179088c76c6b
Luis <luisaraujo.dev@gmail.com>
2025-08-07 15:12:43 -0300

    used useState for monitored statde of editor markdown

--- src/components/Admin/ManegePostForm/index.tsx
+++ src/components/Admin/ManegePostForm/index.tsx
@@ -4,15 +4,24 @@ import { Button } from "@/components/Button";
 import { InputCheckbox } from "@/components/InputCheckbox";
 import { InputText } from "@/components/InputText";
 import { MarkdownEditor } from "@/components/MarkdownEditor";
+import { useState } from "react";
 
 
 export function ManegePostForm() {
+  const [contentValue, setContentValue] = useState();  
+
   return (
     <form action="" className='mb-16'>
           <div className='flex flex-col gap-6'>
             <InputCheckbox labelText='Checkbox' />
 
-            <MarkdownEditor labelText="Conteudo" disabled={false} textAreaName="content" />
+            <MarkdownEditor 
+            labelText="Conteudo" 
+            disabled={false} 
+            textAreaName="content" 
+            value={contentValue}
+            setValue={setContentValue}
+            />
     
             <InputText placeholder='Digite seu nome' id={''} labelText={'Label'} />
             <InputText placeholder='Digite seu nome' id={''} labelText={''} />
