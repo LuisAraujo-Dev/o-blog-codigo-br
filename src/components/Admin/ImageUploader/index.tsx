98269b4f8682bfdda59add97a4bf841d7ca605bb
Luis <luisaraujo.dev@gmail.com>
2025-08-07 16:31:56 -0300

    validating the file size of the new post form on the front-end

--- src/components/Admin/ImageUploader/index.tsx
+++ src/components/Admin/ImageUploader/index.tsx
@@ -1,25 +1,54 @@
 'use clinte'
 
 import { Button } from "@/components/Button";
-import { ImageUpIcon } from "lucide-react";
+import { IMAGE_UPLOADER_MAX_SIZE } from "@/lib/constants";
+import { FileInput, ImageUpIcon } from "lucide-react";
 import { useRef } from "react";
+import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null)
 
   function handleClick() {
-    if(!fileInputRef.current) return 
-    
+    if (!fileInputRef.current) return
+
     fileInputRef.current.click()
-  }; 
-  
+  };
+
+  function handleChange() {
+    if (!fileInputRef.current) return;
+
+    const file = fileInputRef?.current.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOADER_MAX_SIZE / 1024;
+      toast.error(`Imagem muito grande. Max: ${readableMaxSize}KB.`);
+
+      fileInputRef.current.value = '';
+      return;
+    }
+
+    const formData = new FormData()
+    formData.append('file', file)
+
+    fileInputRef.current.value = ''
+  }
+
   return (
     <div className="flex flex-col gap-2 py-4">
       <Button onClick={handleClick} type="button" className="self-start">
-        <ImageUpIcon/>
-          Enviar imagem</Button>
+        <ImageUpIcon />
+        Enviar imagem</Button>
 
-      <input ref={fileInputRef} className='hidden' name='file' type="file" accept="image/*" />
+      <input
+        onChange={handleChange}
+        ref={fileInputRef}
+        className='hidden'
+        name='file'
+        type="file"
+        accept="image/*" />
     </div>
   );
 }
