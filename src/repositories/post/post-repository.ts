daea699c621effaea63dd0e9ded933a68ba070ba
Luis <luisaraujo.dev@gmail.com>
2025-08-21 22:58:33 -0300

    Create update method

--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -9,4 +9,8 @@ export interface PostRepository {
 
   creaate(post: PostModel): Promise<PostModel>
   delete(id: string): Promise<PostModel>
+  update(
+    id: string, 
+    newPostData: Omit<PostModel, 'id' | 'slug' | 'createdAt' | 'updatedAt'>,
+  ): Promise<PostModel>
 }
