aa44d4d99a3078e7da188dfee222bec5624c4624
Luis <luisaraujo.dev@gmail.com>
2025-09-09 16:27:13 -0300

    creating status error notice and creating error notice with toast

--- src/components/Admin/LoginForm/index.tsx
+++ src/components/Admin/LoginForm/index.tsx
@@ -5,44 +5,53 @@ import { Button } from "@/components/Button";
 import { InputText } from "@/components/InputText";
 import clsx from "clsx";
 import { LogInIcon } from "lucide-react";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 export const dynamic = 'force-dynamic';
 
 export function LoginForm() {
   const initialState = {
     username: '',
-    error:'', 
+    error: '',
   }
   const [state, action, isPending] = useActionState(loginAction, initialState)
 
+  useEffect(() => {
+    if (state.error) {
+      toast.dismiss();
+      toast.error(state.error)
+    }
+  }, [state])
 
   return (
     <div className={clsx(
-      'flex items-center justify-center', 
+      'flex items-center justify-center',
       'text-center max-w-sm mt-16 mb-32 mx-auto'
     )}>
       <form action={action} className='flex-1 flex flex-col gap-6'>
-        <InputText 
-        type="text"
-        name="username"
-        labelText="Usuário"
-        placeholder="Seu usuário"
-        disabled={isPending}
-        defaultValue={state.username}
+        <InputText
+          type="text"
+          name="username"
+          labelText="Usuário"
+          placeholder="Seu usuário"
+          disabled={isPending}
+          defaultValue={state.username}
         />
-        <InputText 
-        type="password"
-        name="password"
-        labelText="Senha"
-        placeholder="Sua senha"
-        disabled={isPending}
+        <InputText
+          type="password"
+          name="password"
+          labelText="Senha"
+          placeholder="Sua senha"
+          disabled={isPending}
         />
 
-      <Button disabled={isPending} type='submit' className="mt-4">
-        <LogInIcon />
-        Entrar
-      </Button>
+        <Button disabled={isPending} type='submit' className="mt-4">
+          <LogInIcon />
+          Entrar
+        </Button>
+
+        {!!state.error && <p className="text-red-600">{state.error}</p>}
       </form>
     </div>
   );
