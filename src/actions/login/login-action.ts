5e3552ef512328e27bd5d6ec3d4dfdb724c162fc
Luis <luisaraujo.dev@gmail.com>
2025-09-09 14:12:31 -0300

    transferring the form to a client component

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,7 +1,18 @@
 'use server'
 
 import { asyncDelay } from "@/utils/async-delay"
+import { error } from "console";
 
-export async function loginAction() {
+type LoginActionState = {
+  username: string, 
+  error: string, 
+}
+
+export async function loginAction(state: LoginActionState, formData: FormData) {
   await asyncDelay(3000); 
+
+  return {
+    username: '', 
+    error: ''
+  }
 }
--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,42 +1,7 @@
-import { loginAction } from "@/actions/login/login-action";
-import { Button } from "@/components/Button";
-import { InputText } from "@/components/InputText";
-import clsx from "clsx";
-import { LogInIcon } from "lucide-react";
-import { useActionState } from "react";
+import { LoginForm } from "@/components/Admin/LoginForm";
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminLoginPage() {
-  const [state, action, isPending] = useActionState(loginAction, undefined)
-
-
-  return (
-    <div className={clsx(
-      'flex items-center justify-center', 
-      'text-center max-w-sm mt-16 mb-32 mx-auto'
-    )}>
-      <form action="" className='flex-1 flex flex-col gap-6'>
-        <InputText 
-        type="text"
-        name="username"
-        labelText="Usuário"
-        placeholder="Seu usuário"
-        disabled={isPending}
-        />
-        <InputText 
-        type="password"
-        name="password"
-        labelText="Senha"
-        placeholder="Sua senha"
-        disabled={isPending}
-        />
-
-      <Button disabled={isPending} type='submit' className="mt-4">
-        <LogInIcon />
-        Entrar
-      </Button>
-      </form>
-    </div>
-  );
+  return <LoginForm/>
 }
