4f01fbfeb4c94b18e002a658c8b154b890266a63
Luis <luisaraujo.dev@gmail.com>
2025-09-09 17:12:20 -0300

    Generating and comparing password hashes with Bcrypt for security

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -2,6 +2,7 @@
 
 import { asyncDelay } from "@/utils/async-delay"
 import { error } from "console";
+import { toast } from "react-toastify";
 
 type LoginActionState = {
   username: string, 
@@ -11,8 +12,21 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
   await asyncDelay(3000); 
 
+  if (!(formData instanceof FormData)) {
+    return {
+    username: '', 
+    error: 'Dados inválidos'
+  }
+  } 
+
+  const username = formData.get('username')?.toString || ''; 
+  const password = formData.get('password')?.toString || ''; 
+
+  const isUsernameValid = username === process.env.LOGIN_USER; 
+
+}
+
   return {
     username: '', 
     error: ''
   }
-}
