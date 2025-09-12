67c1511cf93e0c968c67cfbc465de65043b7f681
Luis <luisaraujo.dev@gmail.com>
2025-09-12 17:53:44 -0300

    creating a function to check if the user is authenticated, and redirect if not

--- src/lib/login/manage-login.ts
+++ src/lib/login/manage-login.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import { cookies } from "next/headers";
 import { SignJWT, jwtVerify} from "jose"
+import { redirect } from "next/navigation";
 
 const jwtSecretKey = process.env.JWT_SECRET_KEY; 
 const jwtEncodeKey = new TextEncoder().encode(jwtSecretKey)
@@ -61,6 +62,14 @@ export async function verifyLoginSession() {
   if(!jwtPayload) return false; 
 
   return jwtPayload?.username === process.env.LOGIN_USER; 
+}
+
+export async function requireLoginSessionOrRedirect() {
+  const isAuthententicated = await getLoginSession(); 
+
+  if (!isAuthententicated) {
+    redirect('admin/login')
+  }
 }
 
 export async function signJwt(jwtPayload: JwtPayload) {
