1e57ac9a8219d9a58b62ceff5b2395dd2eab66a8
Luis <luisaraujo.dev@gmail.com>
2025-09-15 17:16:37 -0300

    add logic allowed login in login page

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,16 @@
 import { LoginForm } from "@/components/Admin/LoginForm";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminLoginPage() {
+  const allowLogin = Boolean(Number(process.env.ALLOW_LOGIN))
+
+  if(!allowLogin) {
+    return (
+      <ErrorMessage 
+      contentTitle={"403"} content={"Libere o sistema de login usando o ALLOW_LOGIN"} />
+    )
+  }
   return <LoginForm/>
 }
